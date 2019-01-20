ff488199208f40d1c5e8d447e9d4e8e11ac5250a
Budh Ram Gurung <budhram.gurung01@gmail.com>
2019-01-20 18:20:11 +0530

    Seelect first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// When the user scrolls down 100px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("page-top").style.display = "block";
+  } else {
+    document.getElementById("page-top").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
